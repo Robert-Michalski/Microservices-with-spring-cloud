76ccb13fb597bbd0a22bba4c47fa46f26393c6ae
Robert-Michalski <rmicha@softserveinc.com>
2023-01-12 17:08:01 +0100

    add gateway routes for user and payment services

--- payment-service/src/main/java/com/rob/paymentservice/PaymentApi.java
+++ payment-service/src/main/java/com/rob/paymentservice/PaymentApi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/payment-api")
 public class PaymentApi {
 
     @Value("${eureka.instance.instance-id}")
--- user-service/src/main/java/com/rob/userservice/UserApi.java
+++ user-service/src/main/java/com/rob/userservice/UserApi.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user-api")
 public class UserApi {
 
     @GetMapping
