976ddc92f75fbaba312fe6e8e1ea855623bc46d0
Robert-Michalski <rmicha@softserveinc.com>
2023-01-13 12:28:53 +0100

    add config server with github

--- user-service/src/main/java/com/rob/userservice/UserApi.java
+++ user-service/src/main/java/com/rob/userservice/UserApi.java
@@ -1,5 +1,6 @@
 package com.rob.userservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user-api")
 public class UserApi {
-
+    @Value("${app.user}")
+    private String appUser;
     @GetMapping
     public String hello(){
-        return "Hello from user api";
+        return "Hello from user api its "+appUser;
     }
 }
