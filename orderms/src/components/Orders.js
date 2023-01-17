331a6b08196b5b3e8b791a347c8831183f67521c
Robert-Michalski <rmicha@softserveinc.com>
2023-01-17 11:41:35 +0100

    add react app and Orders view

---
+++ orderms/src/components/Orders.js
@@ -0,0 +1,89 @@
+import React, { useEffect } from "react"
+import SingleOrder from "./SingleOrder"
+function Orders() {
+  return (
+    <div className="d-flex container">
+      {/* Leftie and rightie */}
+      <div className="d-flex flex-column nav mt-4 p-4 bg-dark fc-white">
+        <div className="d-flex">
+          {/* Icon and text */}
+          <div>
+            <span class="material-symbols-outlined">shopping_cart</span>
+          </div>
+        </div>
+        <hr className="bg-white" />
+        <div className="d-flex mt-4 p-2">
+          {/* Icon and text */}
+          <div>
+            <span class="material-symbols-outlined">speed</span>
+          </div>
+          <div className="ms-3 mt-1">DASHBOARD</div>
+        </div>
+        <hr className="bg-white" />
+        <div className="d-flex p-2 mt-2">
+          {/* Icon and text */}
+          <div id="label-icon">
+            <span class="material-symbols-outlined">label</span>
+          </div>
+          <div className="ms-3 mt-2">PRODUCTS</div>
+        </div>
+        <hr className="bg-white" />
+        <div className="d-flex p-2 mt-2">
+          {/* Icon and text */}
+          <div>
+            <span class="material-symbols-outlined">inventory_2</span>
+          </div>
+          <div className="ms-3 mt-1">ORDERS</div>
+        </div>
+        <hr className="bg-white" />
+        <div className="d-flex p-2 mt-2">
+          {/* Icon and text */}
+          <div>
+            <span class="material-symbols-outlined">settings</span>
+          </div>
+          <div className="ms-3 mt-1">SETTINGS</div>
+        </div>
+        <hr className="bg-white" />
+        <div className="d-flex p-2 mt-5">
+          {/* Icon and text */}
+          <div className="mx-auto mt-5">
+            <span class="material-symbols-outlined">logout</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-10 mx-auto p-1 mt-4 bg-gray">
+        <div className="d-flex orders-top p-4 align-items-center">
+          <div className="ms-4">Orders</div>
+          <span class="material-symbols-outlined ms-auto">search</span>
+          <span class="material-symbols-outlined ms-3">notifications</span>
+          <div className="ms-5">John Doe</div>
+        </div>
+        <hr />
+        <div className="container p-3">
+          <div className="d-flex">
+            <div className="ms-3 fs-2">My orders</div>
+            <div className="ms-auto me-4 search-order">
+              <input type="text" placeholder="Find an order"></input>
+            </div>
+          </div>
+          <div className="bg-white row mt-4 ms-2 orders p-3">
+            <div class="col-sm grid-header">Order ID</div>
+            <div class="col-sm grid-header">Item</div>
+            <div class="col-sm grid-header">Buyer</div>
+            <div class="col-sm grid-header">Date</div>
+            <div class="col-sm grid-header">Status</div>
+            <div class="col-sm grid-header">Amount</div>
+            <div className="w-100"></div>
+            <hr className="mt-3" />
+            <SingleOrder />
+            <SingleOrder />
+            <SingleOrder />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Orders
---
+++ orderms/src/components/SingleOrder.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function SingleOrder() {
+  return (
+    <>
+      <div class="col-sm p-3">123123123</div>
+      <div class="col-sm p-3">Personal Computer</div>
+      <div class="col-sm p-3">Robur</div>
+      <div class="col-sm p-3">17 January 2023</div>
+      <div class="col-sm p-3">RECEIVED</div>
+      <div class="col-sm p-3">2350</div>
+      <hr className="mt-2" />
+      <div className="w-100"></div>
+    </>
+  )
+}
+
+export default SingleOrder
