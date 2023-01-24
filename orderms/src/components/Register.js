fc89489c6c52f06782835557c40f50983da532e8
Robert-Michalski <rmicha@softserveinc.com>
2023-01-24 13:27:10 +0100

    make new register form work

--- orderms/src/components/Register.js
+++ orderms/src/components/Register.js
@@ -3,68 +3,150 @@ import { useImmerReducer } from "use-immer"
 import Axios from "axios"
 import { useNavigate } from "react-router"
 import warehousebg from "./img/register-warehouse.jpg"
+import { useContext } from "react"
+import DispatchContext from "../DispatchContext"
 function Register() {
   const navigate = useNavigate()
+  const appDispatch = useContext(DispatchContext)
   const initialState = {
+    requestCount: 0,
     mail: {
       value: "",
-      hasErrors: false
+      hasErrors: true,
+      iconController: {
+        icon: "cancel",
+        color: "red"
+      }
     },
     password: {
       value: "",
-      hasErrors: false
+      hasErrors: true,
+      iconController: {
+        icon: "cancel",
+        color: "red"
+      }
     },
     firstName: {
       value: "",
-      hasErrors: false
+      hasErrors: true,
+      iconController: {
+        icon: "cancel",
+        color: "red"
+      }
     },
     lastName: {
       value: "",
-      hasErrors: false
+      hasErrors: true,
+      iconController: {
+        icon: "cancel",
+        color: "red"
+      }
     },
     phone: {
       value: "",
-      hasErrors: false
+      hasErrors: true,
+      iconController: {
+        icon: "cancel",
+        color: "red"
+      }
     }
   }
   function ourReducer(draft, action) {
     switch (action.type) {
       case "emailImmediately":
-        draft.mail.hasErrors = false
         draft.mail.value = action.value
+        if (/^\S+@\S+$/.test(draft.mail.value)) {
+          draft.mail.iconController.icon = "check_circle"
+          draft.mail.iconController.color = "green"
+          draft.mail.hasErrors = false
+        }
+        if (draft.mail.value.length == 0 || !/^\S+@\S+$/.test(draft.mail.value)) {
+          draft.mail.iconController.icon = "cancel"
+          draft.mail.iconController.color = "red"
+          draft.mail.hasErrors = true
+        }
         return
       case "passwordImmediately":
-        draft.password.hasErrors = false
+        //TODO
+        // WHY IT ALWAYS RETURNS FALSE
+        // const passwordRegExp = /^.*(?=.{7,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!&$%&? "]).*$/
+        // passwordRegExp.test(draft.password.value)
+        //
         draft.password.value = action.value
+        draft.password.hasErrors = draft.password.value.length < 8
+        if (draft.password.hasErrors) {
+          draft.password.iconController.icon = "cancel"
+          draft.password.iconController.color = "red"
+        } else {
+          draft.password.iconController.icon = "check_circle"
+          draft.password.iconController.color = "green"
+        }
         return
       case "firstNameImmediately":
-        draft.firstName.hasErrors = false
         draft.firstName.value = action.value
+        draft.firstName.hasErrors = draft.firstName.value < 1
+        if (draft.firstName.hasErrors) {
+          draft.firstName.iconController.icon = "cancel"
+          draft.firstName.iconController.color = "red"
+        } else {
+          draft.firstName.iconController.icon = "check_circle"
+          draft.firstName.iconController.color = "green"
+        }
         return
       case "lastNameImmediately":
-        draft.lastName.hasErrors = false
         draft.lastName.value = action.value
+        draft.lastName.hasErrors = draft.lastName.value < 1
+        if (draft.lastName.hasErrors) {
+          draft.lastName.iconController.icon = "cancel"
+          draft.lastName.iconController.color = "red"
+        } else {
+          draft.lastName.iconController.icon = "check_circle"
+          draft.lastName.iconController.color = "green"
+        }
         return
       case "phoneImmediately":
-        draft.phone.hasErrors = false
         draft.phone.value = action.value
+        if (draft.phone.value.length == 9) {
+          draft.phone.hasErrors = false
+        } else {
+          draft.phone.hasErrors = true
+        }
+        if (draft.phone.hasErrors) {
+          draft.phone.iconController.icon = "cancel"
+          draft.phone.iconController.color = "red"
+        } else {
+          draft.phone.iconController.icon = "check_circle"
+          draft.phone.iconController.color = "green"
+        }
+        return
+      case "submit":
+        draft.requestCount++
+        return
     }
   }
   const [state, dispatch] = useImmerReducer(ourReducer, initialState)
-  async function handleSubmit(e) {
-    e.preventDefault()
-    const ourRequest = Axios.CancelToken.source()
-    console.log(state)
-    try {
-      // const response = await Axios.post("api/user", { firstName: state.firstName, lastName: state.lastName, phone: state.phone, mail: state.mail, password: state.password }, { cancelToken: ourRequest.token })
-      // if (response.request.status == 201) {
-      //   navigate("/")
-      // }
-    } catch (e) {
-      console.log("something went drong during register " + e)
-    }
-  }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: "submit" })
+  }
+  useEffect(() => {
+    if (state.requestCount) {
+      const fetchData = async () => {
+        const ourRequest = Axios.CancelToken.source()
+        try {
+          const response = await Axios.post("api/user", { firstName: state.firstName.value, lastName: state.lastName.value, phone: state.phone.value, mail: state.mail.value, password: state.password.value }, { cancelToken: ourRequest.token })
+          if (response.request.status == 201) {
+            appDispatch({ type: "flashMessage", value: "User succesfully created" })
+            navigate("/")
+          }
+        } catch (e) {
+          console.log("something went drong during register " + e)
+        }
+      }
+      fetchData()
+    }
+  }, [state.requestCount])
   return (
     <div className="container bg-gray mt-4">
       <form className="d-flex flex-column register mt-5" onSubmit={handleSubmit}>
@@ -79,33 +161,68 @@ function Register() {
                 <span className="ms-4 fs-5 fc-blue ">Log in</span>
               </div>
               <div className="d-flex input-group input-group-register mb-4 ">
-                <input type="text" placeholder="E-mail" className="container" />
-                <div className="material-symbols-outlined" style={{ color: "red" }}>
-                  cancel
+                <input
+                  type="text"
+                  placeholder="E-mail"
+                  className="container"
+                  onChange={e => {
+                    dispatch({ type: "emailImmediately", value: e.target.value })
+                  }}
+                />
+                <div className="material-symbols-outlined" style={{ color: state.mail.iconController.color }}>
+                  {state.mail.iconController.icon}
                 </div>
               </div>
               <div className="d-flex input-group input-group-register mb-4 ">
-                <input type="password" placeholder="Password" className="container" />
-                <div className="material-symbols-outlined" style={{ color: "green" }}>
-                  check_circle
+                <input
+                  type="password"
+                  placeholder="Password"
+                  onChange={e => {
+                    dispatch({ type: "passwordImmediately", value: e.target.value })
+                  }}
+                  className="container"
+                />
+                <div className="material-symbols-outlined" style={{ color: state.password.iconController.color }}>
+                  {state.password.iconController.icon}
                 </div>
               </div>
               <div className="d-flex input-group input-group-register mb-4 ">
-                <input type="text" placeholder="First name" className="container" />
-                <div className="material-symbols-outlined" style={{ color: "green" }}>
-                  check_circle
+                <input
+                  type="text"
+                  placeholder="First name"
+                  onChange={e => {
+                    dispatch({ type: "firstNameImmediately", value: e.target.value })
+                  }}
+                  className="container"
+                />
+                <div className="material-symbols-outlined" style={{ color: state.firstName.iconController.color }}>
+                  {state.firstName.iconController.icon}
                 </div>
               </div>
               <div className="d-flex input-group input-group-register mb-4 ">
-                <input type="text" placeholder="Last name" className="container" />
-                <div className="material-symbols-outlined" style={{ color: "green" }}>
-                  check_circle
+                <input
+                  type="text"
+                  placeholder="Last name"
+                  onChange={e => {
+                    dispatch({ type: "lastNameImmediately", value: e.target.value })
+                  }}
+                  className="container"
+                />
+                <div className="material-symbols-outlined" style={{ color: state.lastName.iconController.color }}>
+                  {state.lastName.iconController.icon}
                 </div>
               </div>
               <div className="d-flex input-group input-group-register mb-4 ">
-                <input type="text" placeholder="Phone" className="container" />
-                <div className="material-symbols-outlined" style={{ color: "green" }}>
-                  check_circle
+                <input
+                  type="text"
+                  placeholder="Phone"
+                  onChange={e => {
+                    dispatch({ type: "phoneImmediately", value: e.target.value })
+                  }}
+                  className="container"
+                />
+                <div className="material-symbols-outlined" style={{ color: state.phone.iconController.color }}>
+                  {state.phone.iconController.icon}
                 </div>
               </div>
               <div className="d-flex">
