f6a68158a0d0ff9da4b86d7550c7903d9b2be143
Robert Michalski <robertm1013@gmail.com>
2023-02-08 13:08:19 +0100

    make jwt valid for one day

--- user-service/src/main/java/com/rob/userservice/config/JWTGenerator.java
+++ user-service/src/main/java/com/rob/userservice/config/JWTGenerator.java
@@ -4,12 +4,15 @@ import com.rob.userservice.service.UserService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -27,10 +30,15 @@ public class JWTGenerator {
 
     private final UserService userService;
 
+    private static final Logger log = LoggerFactory.getLogger(JWTGenerator.class);
+
     public String generateToken(Authentication authentication) {
+
         String username = authentication.getName();
         Date currentDate = new Date();
-        Date expireDate = new Date(currentDate.getTime() + JWT_EXPIRATION);
+        Date expireDate = Date.from(Instant.now().plusSeconds(JWT_EXPIRATION));
+        log.info("JWT_EXPIRATION = {}", JWT_EXPIRATION);
+        log.info("Generating token for user {} valid untill {}", authentication.getName(), expireDate);
         Map<String, Object> claims = new HashMap<>();
         List<String> roles = authentication.getAuthorities()
                 .stream()
