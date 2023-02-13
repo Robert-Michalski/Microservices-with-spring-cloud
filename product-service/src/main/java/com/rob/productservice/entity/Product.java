58ca80d11b31426f33c231a0a0c836be46988a1a
Robert Michalski <robertm1013@gmail.com>
2023-02-13 11:07:25 +0100

    change liquibase population to new products and categories

--- product-service/src/main/java/com/rob/productservice/entity/Product.java
+++ product-service/src/main/java/com/rob/productservice/entity/Product.java
@@ -14,7 +14,6 @@ public class Product {
     private Long id;
     private String name;
     @ManyToOne
-    @JoinColumn(name = "category_id")
     private Category category;
     private double price;
     private String details;
