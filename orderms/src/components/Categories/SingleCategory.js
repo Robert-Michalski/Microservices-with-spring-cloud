bfbf10e56c2528a7658d0d602be8ea98d439bb5d
Robert Michalski <robertm1013@gmail.com>
2023-02-13 14:18:27 +0100

    make frontend decode blob to image

--- orderms/src/components/Categories/SingleCategory.js
+++ orderms/src/components/Categories/SingleCategory.js
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom"
-
+import { Buffer } from "buffer"
 function SingleCategory(props) {
   function getImage() {
     if (props.img) {
       const a = Buffer.from(props.img, "base64")
-      console.log(a)
+      const b = a.toString("base64")
+      return <img src={"data:image/png;base64," + b} alt="Red dot" />
     } else {
-      return "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/geforce-rtx-turing/overview/shop-2080-ti-300@2x.jpg"
+      return <img src="" alt="Red dot" />
     }
   }
 
   return (
     <div className="d-flex category mb-3">
-      <div className="category-img col-5">
-        <img src={getImage()} />
-      </div>
+      <div className="category-img col-2">{getImage()}</div>
       <Link to={"/products?category=" + props.category.name} className="align-self-center fs-2 fw-bold ms-4">
         {props.category.name}
       </Link>
